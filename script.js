cc81bb5da33cc316e931b3ed2690cb4d0e7271e8
Laka Nodia <laka.nodia@gmail.com>
2022-05-11 21:26:51 +0400

    Changed images

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 let data = [
 {
     id: 1,
-    imageUrl: 'https://s3-cdn.designerjourneys.com/blog/wp-content/uploads/2021/01/25080547/john-hernandez-IL-fBBnF-GU-unsplash-2-scaled.jpg',
-    title: 'Philippines',
+    imageUrl: 'https://georgiantravelguide.com/storage/files/ardaganis-tba-batumi-ardagani-lake-batumi-3.jpg',
+    title: 'Batumi',
     url:'https://www.google.com/'
 },
 {
     id: 2,
-    imageUrl: 'https://a.cdn-hotels.com/gdcs/production129/d1719/eaa3ce8a-cb82-4e0c-9b8a-1dda760d14d2.jpg',
-    title: 'Tailand',
+    imageUrl: 'https://scontent.ftbs5-3.fna.fbcdn.net/v/t31.18172-8/17434479_1632400220123360_7318023573805875703_o.jpg?_nc_cat=106&ccb=1-6&_nc_sid=9267fe&_nc_ohc=fAs47GWb_zsAX_SnFIg&_nc_ht=scontent.ftbs5-3.fna&oh=00_AT_ag3v8WI_YP0kmkmDn9EF2LbJyjsyOgQcxR0qUbrrwxw&oe=62A111A4',
+    title: 'Paliastomi',
     url:'https://www.google.com/'
 },
 {
     id: 3,
-    imageUrl: 'https://i2.wp.com/www.alltherooms.com/blog/wp-content/uploads/2018/09/Feature-Beaches-in-the-Maldives-By-Nikolay-007.jpg?fit=1000%2C667&ssl=1',
-    title: 'Maldives',
+    imageUrl: 'https://scontent.ftbs5-3.fna.fbcdn.net/v/t31.18172-8/10344384_963275053702550_5483404344653950204_o.jpg?_nc_cat=109&ccb=1-6&_nc_sid=9267fe&_nc_ohc=6S6jovhLCIkAX_DBVUT&_nc_ht=scontent.ftbs5-3.fna&oh=00_AT-YW8UucOcDtqE_8qJtntrN9yOVToMNgrkyDF75TIAJpQ&oe=62A02DC2',
+    title: 'Tusheti',
+    url:'https://www.google.com/'
+},
+
+{
+    id: 4,
+    imageUrl: 'https://scontent.ftbs5-3.fna.fbcdn.net/v/t31.18172-8/10454355_865190593510997_494723374843033858_o.jpg?_nc_cat=101&ccb=1-6&_nc_sid=9267fe&_nc_ohc=S9PX2LR7ZNwAX-JRx88&tn=e92VBgxIx5ljOt_P&_nc_ht=scontent.ftbs5-3.fna&oh=00_AT9H86hK3kJ36Y5gxpw4ob6QG9eF0Gx6WsLSq3mZamdyPQ&oe=62A2B6C2',
+    title: 'Kazbegi',
     url:'https://www.google.com/'
 },
 {
-    id: 4,
-    imageUrl: 'https://media.cntraveler.com/photos/5e56daf217b1dc0008f2fba6/master/pass/03-waileabeach-hawaii-2020-GettyImages-144924389.jpg',
-    title: 'Hawaii',
+    id: 5,
+    imageUrl: 'https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/237838597_379971683493048_3192286133171823919_n.jpg?_nc_cat=101&ccb=1-6&_nc_sid=730e14&_nc_ohc=4FtB01WmllUAX8gIl14&tn=e92VBgxIx5ljOt_P&_nc_ht=scontent.ftbs5-3.fna&oh=00_AT9IS71UZKgriFjn3VapWNWGjNyi90CC2r8eHNjclOmTCw&oe=62802A60',
+    title: 'Svaneti',
     url:'https://www.google.com/'
-}
+},
 ];
+
 
 let rightArrow = document.getElementById('rightArrow');
 let leftArrow = document.getElementById('leftArrow');
@@ -119,8 +127,8 @@ function rightArrowClick(){
 leftArrow.addEventListener('click', leftArrowClick);
 rightArrow.addEventListener('click', rightArrowClick);
 
-setInterval(() => {
-    rightArrowClick();
-}, 3000);
+// setInterval(() => {
+//     rightArrowClick();
+// }, 3000);
 
 setSlider();
